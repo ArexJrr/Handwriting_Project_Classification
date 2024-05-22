aa932430183b272514b9f366bdcfe3138b88f038
Andrea Pietro Arena <161145872+ArexJrr@users.noreply.github.com>
2024-05-22 19:55:10 +0200

    Implemented a second part of functions in the HWDataset class and a good part of __getitem__

--- data_classes/cds_.py
+++ data_classes/cds_.py
@@ -1,34 +1,94 @@
 from torch.utils.data import Dataset, DataLoader
 import pandas as pd 
 import numpy as np
-import os 
+import os
+import torch 
 import warnings
+import re
+from sklearn.preprocessing import LabelEncoder
 warnings.filterwarnings("ignore", category=pd.errors.DtypeWarning)
 
 
 class HWDataset(Dataset):
-    def __init__(self, data_paths, type_ds):
-        self.data_paths = data_paths
-        self.data_paths[0] = os.path.join(os.path.dirname(__file__), '..', data_paths[0]) # Path Datasets
-        self.data_paths[1] = os.path.join(os.path.dirname(__file__), '..', data_paths[1]) # Path Labels 
-        self.empty_subject_tasks , self.max_len_series = self.chk_csv_data(self.data_paths[0]) # Ritorna una lista con i soggetti failed più il numero massimo della serie
+    def __init__(self, paths, type_ds):
+        self.paths = paths
+        self.paths[0] = os.path.join(os.path.dirname(__file__), '..', paths[0]) # Path Datasets
+        self.paths[1] = os.path.join(os.path.dirname(__file__), '..', paths[1]) # Path Labels 
+        self.empty_subject_tasks , self.max_len_series = self.chk_csv_data(self.paths[0]) # Ritorna una lista con i soggetti failed più il numero massimo della serie
         self.list_valid_subjects = self.remove_empty_subject_tasks()
         self.list_ofsubject = self.select_sub_ds(type_ds)
+        self.data_files = self.data_filecsv_path()
 
     def __len__(self):
-        return len(self.data_paths[0])
+        return len(self.list_valid_subjects)
     
-    def __getitem__(self, index):
-        pass
-    
-    def ml_csv_labels(self, file_name):
-        df = pd.read_csv(file_name, sep=',')
-        return df.iloc[:,[0,-2]]
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        task_id , subject_code = self.identifier_sub_task(self.data_files[idx])
+        label = self.find_label(task_id, subject_code)
+        csv_file = self.data_files[idx]
+        dataset = pd.read_csv(csv_file, sep=',', names=range(21))
+        list_columns_name = dataset.iloc[0]
+        dataset = dataset[1:]
+        dataset.columns = list_columns_name
+        dataset = dataset.iloc[:, :-2].drop(columns=['Sequence'])
+        dataset['Phase'] = LabelEncoder().fit_transform(dataset['Phase']) # LabelEncoding 
+        # Padding 
+        # Time Series feature convertirli
+        # Normalizzazione dei dati 
+
+        item = {
+            'label': torch.tensor(label, dtype=torch.long), #perché si ha compatibilità con la crossentropy loss 
+            'data': torch.tensor(dataset.values, dtype=torch.float32) 
+        }
+        return item
+
+    def find_label(self, task_id, subject_code):
+        csv_template_filename = f'task_{task_id}.csv'
+        csv_file_path = os.path.join(self.paths[1], csv_template_filename)
+        if os.path.isfile(csv_file_path):
+            df = pd.read_csv(csv_file_path, sep=',')
+        else: raise Exception("Label file task_{task_id}.csv not found")
+        label = int(df[df.iloc[:, 0] == subject_code].iloc[:, [0,-2]].iloc[0,1])
+        if label == -1 : raise Exception("Wrong label selected -1 failed task to labelfile")
+        return label 
+
+    def identifier_sub_task(self, path): 
+        normalized_path = os.path.normpath(path)
+        directory, filename = os.path.split(normalized_path)
+        subject_code_match = re.search(r'CRC_SUBJECT_(\d+)', directory)
+        if subject_code_match:
+            subject_code = subject_code_match.group(1)
+        else:
+            raise Exception("Problem folder name: not found CRC_SUBJECT_000 Number")
+
+        task_id = filename.split('_')[0]
+
+        if subject_code.startswith('0'):
+            subject_code = int(str(int(subject_code)))
+        task_id =  re.search(r'Task(\d+)', task_id)
+        if task_id:
+            task_id = int(task_id.group(1))
+
+        return task_id, subject_code
+
+
+    def data_filecsv_path(self):
+        data_files = []
+        for subject_dir in self.list_ofsubject:
+            subject_path = os.path.join(self.paths[0], subject_dir)
+            if os.path.isdir(subject_path):
+                for task_file in os.listdir(subject_path):
+                    if task_file.endswith('.csv'):
+                        file_path = os.path.join(subject_path, task_file)
+                        data_files.append(file_path)
+        return data_files
 
     def remove_empty_subject_tasks(self):
             subfolders = [
-                  entry for entry in os.listdir(self.data_paths[0])
-                    if os.path.isdir(self.data_paths[0])
+                  entry for entry in os.listdir(self.paths[0])
+                    if os.path.isdir(self.paths[0])
                  ]
             new_subfolders = [sub for sub in subfolders if sub not in self.empty_subject_tasks]
             if '.DS_Store' in new_subfolders: new_subfolders.remove('.DS_Store')
@@ -71,12 +131,66 @@ class HWDataset(Dataset):
                         max_n_records = np.maximum(max_n_records, df.index[-1])
                 if not files_in_sub_folder:
                     empty_subfs.append(sub_folder)
-        return empty_subfs, max_n_records
+        return empty_subfs, max_n_records-1 # -1 perché la prima riga è la column_list
 
 
 if __name__ == '__main__':
     dir = []
     dir.append(os.path.join(os.path.dirname(__file__), '..', 'DATA')) # Datasets
     dir.append(os.path.join(os.path.dirname(__file__), '..', 'Labels')) # Labels 
+    #print(dir[1])
+    # test = HWDataset(dir, 'train')
+    # list = test.getdata()
+    # print(list[0])
 
-    test = HWDataset(dir, 'TRAIN')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #stri_ng = "/Users/arexjr/Desktop/ML Project/HWProject/FolderPj/data_classes/../DATA/CRC_SUBJECT_032/Task14_2_11_2023_9_24_59.csv"
+    # normalized_path = os.path.normpath(stri_ng)
+    # directory, filename = os.path.split(normalized_path)
+    # subject_code_match = re.search(r'CRC_SUBJECT_(\d+)', directory)
+    # if subject_code_match:
+    #     subject_code = subject_code_match.group(1)
+    # else:
+    #     raise Exception("Problem folder name: not found CRC_SUBJECT_000 Number")
+
+    # task_id = filename.split('_')[0]
+
+
+    # if subject_code.startswith('0'):
+    #     subject_code = int(str(int(subject_code)))
+    
+    # task_id =  re.search(r'Task(\d+)', task_id)
+    # if task_id:
+    #     task_id = int(task_id.group(1))
+
+    # print(f'Codice soggetto: {subject_code}')
+    # print(f'Nome task: {task_id}')
+    
